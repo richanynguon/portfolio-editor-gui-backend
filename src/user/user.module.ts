88863c78e121a5525a9191c99c037ff29f77d0ea
richanynguon <alina.nguon@gmail.com>
2019-11-29 05:50:31 -0500

    import typeorm feature of userrepo into user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserResolver } from './user.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   providers: [UserResolver]
 })
-export class UserModule {}
+export class UserModule { }
