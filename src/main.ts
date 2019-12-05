4792e43e70a2241ea270c707a0f0e516102fb97d
richanynguon <alina.nguon@gmail.com>
2019-12-05 04:48:47 -0500

    add customer headers

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,6 @@ async function bootstrap() {
   app.use(helmet());
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://richanynguon.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
   });
   app.use(session({
