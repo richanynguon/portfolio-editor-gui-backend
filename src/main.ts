1ce7817fc38c3dd9fa27b448a0d196a2d8eef121
richanynguon <alina.nguon@gmail.com>
2019-12-05 06:50:48 -0500

    revert back

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,32 @@ import * as session from 'express-session'
 import { SESSION_SECRET } from './constants';
 import * as Store from 'connect-redis';
 import { redis } from './redis';
-// import * as helmet from 'helmet';
-
+import * as helmet from 'helmet';
+import * as cors from 'cors';
 
 dotenv.config()
 async function bootstrap() {
   const RedisStore = Store(session);
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(cors({
+    origin: "https://www.richanynguon.com",
+    credentials: true
+  }))
   app.use(session({
     store: new RedisStore({
       client: redis as any,
     }),
     name: "portfolio",
     secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
+    resave: true,
+    saveUninitialized: true,
     cookie: {
-      httpOnly: true,
-      secure: false,
-      maxAge: 1000 * 60 * 60 * 24,
-
-    },
+      httpOnly: false,
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24
+    }
   }))
-
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
