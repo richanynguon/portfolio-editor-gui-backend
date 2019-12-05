b75a5de9a4a2d9de4c331940f53f70e37ea13977
richanynguon <alina.nguon@gmail.com>
2019-12-05 04:41:10 -0500

    add customer headers

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,13 @@ dotenv.config()
 async function bootstrap() {
   const RedisStore = Store(session);
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://richanynguon.com',
-    credentials: true
-  });
   app.use(helmet());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://richanynguon.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   app.use(session({
     store: new RedisStore({
       client: redis as any,
